6b22f9042ebfcd18058d8c5f7176175e55132bcd
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2021-01-25 15:29:31 +0100

    Add publishing

--- lib/src/main/kotlin/de/kevcodez/gotrue/GoTrueDefaultClient.kt
+++ lib/src/main/kotlin/de/kevcodez/gotrue/GoTrueDefaultClient.kt
@@ -1,11 +1,18 @@
 package de.kevcodez.gotrue
 
 import de.kevcodez.gotrue.http.GoTrueHttpClientApache
+import de.kevcodez.gotrue.http.GoTrueHttpClient
 import de.kevcodez.gotrue.json.GoTrueJsonConverterJackson
+import de.kevcodez.gotrue.json.GoTrueJsonConverter
 import org.apache.hc.client5.http.impl.classic.HttpClients
 
 val goTrueJsonConverter = GoTrueJsonConverterJackson()
 
+/**
+ * The default client uses Apache HTTP client 5.x and Jackson FasterXML for DTO conversion.
+ *
+ * If you want to customize, implement [GoTrueHttpClient] and [GoTrueJsonConverter].
+ */
 class GoTrueDefaultClient(
         baseUrl: String,
         defaultHeaders: Map<String, Any>
